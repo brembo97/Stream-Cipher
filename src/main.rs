e81d8ffde2eab8949de33a1b067ee689570221ff
Emilio Brenes <emilio.brenes.p@hotmail.com>
2020-12-01 20:57:49 -0600

    Changed basic alphabet for ascii characters, changed key expansion logic to be decryptable

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,57 @@
 fn main() {
     //TO DO: change alphabet for ASCII numeric values
     // Create col shuffle function
-    let alphabet:[[char;2];13] = [
-        ['a','b'],
-        ['c','d'],
-        ['e','f'],
-        ['g','h'],
-        ['i','j'],
-        ['k','l'],
-        ['m','n'],
-        ['o','p'],
-        ['q','r'],
-        ['s','t'],
-        ['u','v'],
-        ['w','x'],
-        ['y','z']
+    // let alphabet:[[char;2];13] = [
+    //     ['a','b'],
+    //     ['c','d'],
+    //     ['e','f'],
+    //     ['g','h'],
+    //     ['i','j'],
+    //     ['k','l'],
+    //     ['m','n'],
+    //     ['o','p'],
+    //     ['q','r'],
+    //     ['s','t'],
+    //     ['u','v'],
+    //     ['w','x'],
+    //     ['y','z']
+    // ];
+    let alphabet:[[u8;10];12] = [
+        [27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
+        [37, 38, 39, 40, 41, 42, 43, 44, 45, 46],
+        [47, 48, 49, 50, 51, 52, 53, 54, 55, 56],
+        [57, 58, 59, 60, 61, 62, 63, 64, 65, 66],
+        [47, 48, 49, 50, 51, 52, 53, 54, 55, 56],
+        [57, 58, 59, 60, 61, 62, 63, 64, 65, 66],
+        [67, 68, 69, 70, 71, 72, 73, 74, 75, 76],
+        [77, 78, 79, 80, 81, 82, 83, 84, 85, 86],
+        [87, 88, 89, 90, 91, 92, 93, 94, 95, 96],
+        [97, 98, 99, 100, 101, 102, 103, 104, 105, 106],
+        [107, 108, 109, 110, 111, 112, 113, 114, 115, 116],
+        [117, 118, 119, 120, 121, 122, 123, 124, 125, 126]
     ];
-    let seed = "092810";
+
+    let seed = "143593";
     let keystream = alphabet_shuffle(&seed, alphabet.clone());
 
-    let message = "heuristicaysolocomidadelabuena";
-    let encrypted_message = stream_cipher(message, &alphabet, &keystream);
+    let message = String::from("Soy Ivan, estudiante de bachillerato en un instituto de Madrid. Hace una semana estuve de viaje en Sevilla con unos amigos. Teniamos muchas ganas de ir porque nos gusta mucho el sur de Espana. La gente es muy amable y hospitalaria. Sevilla es una de las ciudades mas bonitas de Espana y de Europa. Sus monumentos mas famosos son la Giralda, La Catedral y la Torre del Oro. La Giralda esta situada junto a la Catedral. Es una torre muy alta, y sus alrededores siempre estan llenos de turistas que toman fotografias.");
+    let encrypted_message = stream_cipher(&message, &keystream);
+    let decrypted_message = stream_cipher(&encrypted_message, &keystream);
 
-    assert_eq!(message.len(), encrypted_message.len());
-    println!("{}", encrypted_message);
+    assert_eq!(&message.len(), &encrypted_message.len());
+    println!("{}", &encrypted_message);
+    println!("{}", &decrypted_message);
+    assert_eq!(&message, &decrypted_message);
 }
 
-fn stream_cipher(message: &str, alphabet: &[[char;2];13], keystream: &[[char;2];13]) -> String{
+fn stream_cipher(message: &str, keystream: &[[u8;10];12]) -> String{
 
     let mut result = String::from("");
-
+    let row_size = keystream.len();
+    let col_size = keystream[0].len();
     // Encrypt every character
-    for c in message.chars(){
-        let index = find_letter(&c, alphabet);
-        let key_char = keystream[index.0][index.1];
+    for (i, c) in message.chars().enumerate(){
+        let key_char = keystream[i % row_size][i % col_size] as char;
 
         //Perform XOR
         let xor_bytes = xor(c.to_string().as_bytes(), key_char.to_string().as_bytes());
@@ -42,6 +60,10 @@ fn stream_cipher(message: &str, alphabet: &[[char;2];13], keystream: &[[char;2];
     }
     result
 }
+//
+// fn decrypt(cyphertext: &str, keystream: &[[u8;10];12], alphabet: &[[u8;10];12]){
+//
+// }
 
 fn xor(bytes_1:&[u8], bytes_2:&[u8]) -> Vec<u8>{
     let xor_bytes = bytes_1
@@ -53,7 +75,7 @@ fn xor(bytes_1:&[u8], bytes_2:&[u8]) -> Vec<u8>{
     xor_bytes
 }
 
-fn alphabet_shuffle(seed: &str, mut alphabet: [[char;2]; 13]) -> [[char;2];13]{
+fn alphabet_shuffle(seed: &str, mut alphabet: [[u8;10]; 12]) -> [[u8;10];12]{
     assert_eq!(seed.len(), 6);
 
     // Parse seed
@@ -63,19 +85,19 @@ fn alphabet_shuffle(seed: &str, mut alphabet: [[char;2]; 13]) -> [[char;2];13]{
     }
     // Shuffle operations
     row_swap(digits[0],digits[1], &mut alphabet);
-    row_shuffle(digits[1], digits[2], &mut alphabet);
-    col_swap(digits[3], digits[4], &mut alphabet);
+    row_shuffle(digits[2], digits[3], &mut alphabet);
+    col_swap(digits[4], digits[5], &mut alphabet);
 
     alphabet
 }
 
-fn row_swap(origin_index: usize, new_index: usize, alphabet: &mut [[char; 2]; 13]){
+fn row_swap(origin_index: usize, new_index: usize, alphabet: &mut [[u8; 10]; 12]){
     let tmp = alphabet[origin_index];
     alphabet[origin_index] = alphabet[new_index];
     alphabet[new_index] = tmp;
 }
 
-fn row_shuffle(n_shifts: usize, n_row: usize, alphabet: &mut [[char;2];13]){
+fn row_shuffle(n_shifts: usize, n_row: usize, alphabet: &mut [[u8;10];12]){
 
     let mut row =  alphabet[n_row];
 
@@ -86,7 +108,7 @@ fn row_shuffle(n_shifts: usize, n_row: usize, alphabet: &mut [[char;2];13]){
     }
 }
 
-fn col_swap(origin_index: usize, new_index: usize, alphabet: &mut [[char;2];13]){
+fn col_swap(origin_index: usize, new_index: usize, alphabet: &mut [[u8;10];12]){
 
     for row in alphabet.iter_mut(){
         let tmp = row[origin_index];
@@ -95,14 +117,28 @@ fn col_swap(origin_index: usize, new_index: usize, alphabet: &mut [[char;2];13])
     }
 }
 
-fn find_letter(character: &char, alphabet: &[[char;2];13]) -> (usize, usize){
+// fn find_letter(character: &char, alphabet: &[[char;2];13]) -> (usize, usize){
+//
+//     for (i, &item) in alphabet.iter().enumerate(){
+//         if item[0] == *character{
+//             return (i, 0 as usize)
+//         }else if item[1] == *character{
+//             return (i, 1 as usize)
+//         }
+//     }
+//     (100,100)
+// }
 
-    for (i, &item) in alphabet.iter().enumerate(){
-        if item[0] == *character{
-            return (i, 0 as usize)
-        }else if item[1] == *character{
-            return (i, 1 as usize)
-        }
-    }
-    (100,100)
-}
+// fn find_u8(character: &char, alphabet: &[[u8;10];12]) -> (usize, usize){
+//
+//     let mut index= (100, 100);
+//     for(i, &row) in alphabet.iter().enumerate(){
+//         for (j, &code) in row.iter().enumerate() {
+//             if *character == code as char{
+//                 index = (i, j);
+//                 break;
+//             }
+//         }
+//     }
+//     index
+// }
